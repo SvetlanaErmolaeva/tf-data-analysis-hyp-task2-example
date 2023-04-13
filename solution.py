33c5b9577934ac86623b60424aa7a522037d7b9c
SvetlanaErmolaeva <61152365+SvetlanaErmolaeva@users.noreply.github.com>
2023-04-13 11:03:43 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,10 @@ import numpy as np
 chat_id = 411069874 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
+    flag = True
+    if np.array_equal(x,y):
+        flag = False
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return True
+    return flag
